6a81f44b76dc8e0265f92803148251d030b63927
Theogalh <thomas.bouillon@epitech.eu>
2019-08-25 18:10:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def on_message(message):
     msg = 'Hello {0.author.mention} \n' \
           'Pour poster un message dans ce channel, ton message doit commencer par [LFG]\n' \
           'Seules les annonces sont acceptées, merci de ne pas répondre dans ce channel, ' \
-          'mais répondre dans un autre channel ou en MP.\nVoici ton message:\n\'.format(message)
+          'mais répondre dans un autre channel ou en MP.\nVoici ton message:\n'.format(message)
     await message.author.send(msg)
     await message.author.send(message.content)
     await message.delete()
